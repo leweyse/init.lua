34b1c03269bbeb44e9aa8c99ee3a2cdfd39d31f1
leweyse <darylcastro67@gmail.com>
2026-06-21 10:44:16 +0200

    fix(telescope): enable treesitter

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,13 +3,17 @@ return {
   tag = "0.1.8",
   dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
-    require("telescope").setup({
-      defaults = {
-        preview = {
-          treesitter = false,
-        },
-      },
-    })
+    require('telescope').setup({})
+
+    local preview_utils = require("telescope.previewers.utils")
+    preview_utils.ts_highlighter = function(bufnr, ft)
+      local lang = vim.treesitter.language.get_lang(ft) or ft
+      if not lang or lang == "" then
+        return false
+      end
+
+      return pcall(vim.treesitter.start, bufnr, lang)
+    end
 
     local builtin = require("telescope.builtin")
     vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
